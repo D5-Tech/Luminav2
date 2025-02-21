af9c7bc3919e678b3468d393422f942d30cf0378
Devanarayanan <github899@gmail.com>
2025-02-22 00:10:17 +0530

    scrch pageg madeed, not connected

---
+++ busscrch/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'bus-finder-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch event handler
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version or fetch new
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event handler - cleanup old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
