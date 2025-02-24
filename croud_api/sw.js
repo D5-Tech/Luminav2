a8db6e40955b79051080f7036079f1f316e08b83
Devanarayanan <github899@gmail.com>
2025-02-25 01:06:32 +0530

    croud file created

---
+++ croud_api/sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'bus-crowd-monitor-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Skip ThingSpeak API calls for caching
+  if (event.request.url.includes('api.thingspeak.com')) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => {
+        // If both network and cache fail, show offline page
+        if (event.request.mode === 'navigate') {
+          return caches.match('/');
+        }
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
